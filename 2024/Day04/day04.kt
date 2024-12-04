c1609cc4cfe1cf88a98db0598d07976af3fe4aac
Filippo <corti.filippo03@gmail.com>
2024-12-04 10:50:02 +0100

    Day04 refractor

--- 2024/Day04/day04.kt
+++ 2024/Day04/day04.kt
@@ -3,12 +3,6 @@ import kotlin.math.abs
 
 val XMAS = listOf('X', 'M', 'A', 'S')
 
-fun nextChar(c : Char) : Char? {
-    val index = XMAS.indexOf(c)
-    if (index == -1 || index == XMAS.size - 1) return null
-    return XMAS[index + 1]
-}
-
 val directions = listOf(
     0 to 1,
     0 to -1,
@@ -42,24 +36,40 @@ fun part1(input : List<List<Char>>) : Int {
     for (i in input.indices) {
         for (j in input[0].indices) {
             if (input[i][j] == 'X') {
-                for (dir in directions) {
-                    
-                    val newI = i + dir.first
-                    val newJ = j + dir.second
-
-                    if (newI < 0 || newI >= input.size || newJ < 0 || newJ >= input[0].size)
-                        continue
-                    
-                    if (input[newI][newJ] == 'M') {
-                        count += findXMASDirected(input, newI, newJ, 'A', dir)
-                    }
-                }
+                count += findXMAS(input, i, j, 'M', 0 to 0)
             }
         }
     }
 
     return count
 } 
+
+fun findXMAS(input : List<List<Char>>, i : Int, j : Int, lookFor : Char, dir : Pair<Int, Int>) : Int {
+    var count = 0
+    var dirs = listOf<Pair<Int,Int>>()
+
+    if (lookFor == 'M')
+        dirs = directions
+    else 
+        dirs = listOf(dir)
+
+    for (dir in dirs) {
+        val newI = i + dir.first
+        val newJ = j + dir.second
+
+        if (newI < 0 || newI >= input.size || newJ < 0 || newJ >= input[0].size)
+            continue
+
+        if (input[newI][newJ] != lookFor) 
+            continue
+            
+        if (lookFor == 'S')
+            return 1
+        
+        count += findXMAS(input, newI, newJ, XMAS[XMAS.indexOf(lookFor) + 1], dir)
+    }
+    return count
+}
 
 fun part2(input : List<List<Char>>) : Int {
     var count = 0
@@ -68,28 +78,7 @@ fun part2(input : List<List<Char>>) : Int {
         for (j in input[0].indices) {
             if (input[i][j] == 'A') {
                     
-                //Diagonal 1
-                val neighbour1 = neighbour(input, i, j, -1 to -1)
-                val neighbour2 = neighbour(input, i, j, 1 to 1)
-
-                if (neighbour1 == null || neighbour2 == null) 
-                    continue
-
-                val pattern = "$neighbour1$neighbour2"
-
-                if (pattern != "MS" && pattern != "SM")
-                    continue
-                
-                //Diagonal 2
-                val neighbour3 = neighbour(input, i, j, -1 to 1)
-                val neighbour4 = neighbour(input, i, j, 1 to -1)
-
-                if (neighbour3 == null || neighbour4 == null) 
-                    continue
-
-                val pattern2 = "$neighbour3$neighbour4"
-
-                if (pattern2 == "MS" || pattern2 == "SM")
+                if (findMS(input, i, j, (-1 to -1) to (1 to 1)) && findMS(input, i, j, (-1 to 1) to (1 to -1))) 
                     count++
             }
         }
@@ -98,23 +87,16 @@ fun part2(input : List<List<Char>>) : Int {
     return count
 }
 
-fun findXMASDirected(input : List<List<Char>>, i : Int, j : Int, lookFor : Char, dir : Pair<Int, Int>) : Int {
-    var count = 0
-    val nextLookFor = nextChar(lookFor)
+fun findMS(input : List<List<Char>>, i : Int, j : Int, dirs : Pair<Pair<Int, Int>, Pair<Int, Int>>) : Boolean {
+    val neighbour1 = neighbour(input, i, j, dirs.first)
+    val neighbour2 = neighbour(input, i, j, dirs.second)
 
-    val newI = i + dir.first
-    val newJ = j + dir.second
+    if (neighbour1 == null || neighbour2 == null) 
+        return false
 
-    if (newI < 0 || newI >= input.size || newJ < 0 || newJ >= input[0].size)
-        return 0
+    val pattern = "$neighbour1$neighbour2"
 
-    if (input[i + dir.first][j + dir.second] == lookFor) {
-        if (nextLookFor != null)
-            count += findXMASDirected(input, i + dir.first, j + dir.second, nextLookFor, dir)
-        else
-            count++
-    }
-    return count
+    return (pattern == "MS" || pattern == "SM")
 }
 
 // kotlinc day04.kt -include-runtime -d day04.jar
