58f89fd9c3ac68f6dee500dfd87d3fd667c0629d
Filippo <corti.filippo03@gmail.com>
2024-12-06 18:28:07 +0100

    :/

--- 2024(Kotlin)/Day06/day06.kt
+++ 2024(Kotlin)/Day06/day06.kt
@@ -38,12 +38,9 @@ fun main() {
     val cols = lines[0].length
 
 
-    //println(guard)
-    //println(obstructions)
 
-    println(part1(obstructions, guard, rows, cols))
-    // println(part2(rules, updates))
-    //println(part2(text))
+    //println(part1(obstructions, guard, rows, cols))
+    println(part2(obstructions, guard, rows, cols)) //1767
 }
 
 
@@ -56,8 +53,6 @@ fun part1(obstructions : HashSet<Pair<Int, Int>>, guard : Pair<Int, Int>, rows :
     occupied.add(curr)
     while (true) {
         val next = Pair(curr.first + dir.first, curr.second + dir.second)
-
-        //println("Next is $next")
 
         if (next.first < 0 || next.first >= rows || next.second < 0 || next.second >= cols)
             return occupied.size
@@ -74,19 +69,71 @@ fun part1(obstructions : HashSet<Pair<Int, Int>>, guard : Pair<Int, Int>, rows :
     return occupied.size
 } 
 
-fun isValidUpdate(order : List<Int>, update : List<Int>) : Boolean {
-    var lastIndex = order.indexOf(update[0])
-    for (i in 1 until update.size) {
-        val currentIndex = order.indexOf(update[i])
-        if (currentIndex <= lastIndex) 
-            return false
-        lastIndex = currentIndex
+fun part2(obstructions : HashSet<Pair<Int, Int>>, guard : Pair<Int, Int>, rows : Int, cols : Int) : Int  {
+    
+    val occupied : HashSet<Pair<Pair<Int, Int>, Pair<Int, Int>>> = HashSet()
+    val positionsToFind : HashSet<Pair<Int, Int>> = HashSet()
+    var dir = -1 to 0
+    var curr = guard
+
+    var i = 0
+    while (true) {
+        val next = Pair(curr.first + dir.first, curr.second + dir.second)
+
+        if (next.first < 0 || next.first >= rows || next.second < 0 || next.second >= cols) {
+            //println(positionsToFind)
+            return positionsToFind.size
+        }
+
+        //println("$i")
+        i++
+
+        if (obstructions.contains(next)) {
+            dir = turnRight(dir)
+        } else {
+            val newObstructions = obstructions.toMutableSet()
+            newObstructions.add(next)
+            if (isLooping(newObstructions.toHashSet(), Pair(curr, turnRight(dir)), rows, cols))
+                positionsToFind.add(next)
+
+            curr = next
+        }
+
     }
-    return true
+
+    return positionsToFind.size
 }
 
-fun part2(rules : List<String>, updates : List<String>) : Int {
-    return 0
+fun isLooping(obstructions : HashSet<Pair<Int, Int>>, guard : Pair<Pair<Int, Int>, Pair<Int, Int>>, rows : Int, cols : Int) : Boolean {
+    
+    val occupied : HashSet<Pair<Pair<Int, Int>, Pair<Int, Int>>> = HashSet()
+    var curr = guard.first
+    var dir = guard.second
+
+    occupied.add(guard)
+    while (true) {
+        val next = Pair(curr.first + dir.first, curr.second + dir.second)
+
+        //println("Next is $next $dir")
+
+        if (next.first < 0 || next.first >= rows || next.second < 0 || next.second >= cols)
+            return false
+
+        if (obstructions.contains(next)) {
+            dir = turnRight(dir)
+        } else {
+            if (occupied.contains(Pair(next, dir))) 
+                return true
+
+            curr = next
+
+            occupied.add(Pair(curr, dir))
+        }
+        
+    }
+
+    return false
 }
+
 // kotlinc day06.kt -include-runtime -d day06.jar
 // java -jar day06.jar
