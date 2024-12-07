6bdfbb8136e86b2b5f5f0d39b44caef52984e2b8
Filippo <corti.filippo03@gmail.com>
2024-12-07 10:36:10 +0100

    Day07 refractor

--- 2024(Kotlin)/Day07/day07.kt
+++ 2024(Kotlin)/Day07/day07.kt
@@ -2,6 +2,19 @@ import java.io.File
 
 data class Equation (val res : Long, val operands : List<Long>)
 
+
+fun sum(l1 : Long, l2 : Long) : Long {
+    return l1 + l2
+}
+
+fun prod(l1 : Long, l2 : Long) : Long {
+    return l1 * l2
+}
+ 
+fun concat(l1 : Long, l2 : Long) : Long {
+    return "$l1$l2".toLong()
+}
+
 fun main() {
 
     val lines = File("input.txt").readLines().map {
@@ -11,80 +24,63 @@ fun main() {
         Equation(res, operands)
     }
 
-    //println(lines)
-
     println(part1(lines))
     println(part2(lines))
-}
-
-fun solveWithPlus(eq : Equation) : Boolean {
-    //println("Trying to solve with + $eq")
-    if (eq.res < 0) return false
-    if (eq.operands.size == 2) return eq.res == eq.operands[0] + eq.operands[1]
-    val plus = eq.operands[0] + eq.operands[1]
-    val plusEq = Equation(eq.res, eq.operands.drop(2).toMutableList().apply { add(0, plus) }.toList())
-    return isSolveable1(plusEq)
-}
-
-fun solveWithProd(eq : Equation) : Boolean {
-   // println("Trying to solve with * $eq")
-    if (eq.res < 0) return false
-    if (eq.operands.size == 2) return eq.res == eq.operands[0] * eq.operands[1]
-    val prod = eq.operands[0] * eq.operands[1]
-    
-    val prodEq = Equation(eq.res, eq.operands.drop(2).toMutableList().apply { add(0, prod) }.toList())
-
-    return isSolveable1(prodEq)
-}
-
-fun isSolveable1(eq : Equation) : Boolean {
-    return solveWithPlus(eq) || solveWithProd(eq)
 }
 
 fun part1(lines : List<Equation>) : Long {
    return lines.filter(::isSolveable1).map { it.res }.sum()
 } 
 
-fun concat(l1 : Long, l2 : Long) : Long {
-    return "$l1$l2".toLong()
-}
-
-fun solveWithConcat(eq : Equation) : Boolean {
-    if (eq.res < 0) return false
-    if (eq.operands.size == 2) return eq.res == concat(eq.operands[0], eq.operands[1])
-    val conc = concat(eq.operands[0], eq.operands[1])
-    
-    val prodEq = Equation(eq.res, eq.operands.drop(2).toMutableList().apply { add(0, conc) }.toList())
-
-    return isSolveable2(prodEq)
-}
-
-fun solveWithPlus2(eq : Equation) : Boolean {
-    //println("Trying to solve with + $eq")
-    if (eq.res < 0) return false
-    if (eq.operands.size == 2) return eq.res == eq.operands[0] + eq.operands[1]
-    val plus = eq.operands[0] + eq.operands[1]
-    val plusEq = Equation(eq.res, eq.operands.drop(2).toMutableList().apply { add(0, plus) }.toList())
-    return isSolveable2(plusEq)
-}
-
-fun solveWithProd2(eq : Equation) : Boolean {
-   // println("Trying to solve with * $eq")
-    if (eq.res < 0) return false
-    if (eq.operands.size == 2) return eq.res == eq.operands[0] * eq.operands[1]
-    val prod = eq.operands[0] * eq.operands[1]
-    
-    val prodEq = Equation(eq.res, eq.operands.drop(2).toMutableList().apply { add(0, prod) }.toList())
-
-    return isSolveable2(prodEq)
-}
-
-fun isSolveable2(eq : Equation) : Boolean {
-    return solveWithPlus2(eq) || solveWithProd2(eq) || solveWithConcat(eq)
-}
-
 fun part2(lines : List<Equation>) : Long  {
    return lines.filter(::isSolveable2).map { it.res }.sum()
 }
+
+fun solveWithOp1(eq : Equation, op : (Long, Long) -> Long) : Boolean {
+    if (eq.res < 0 || eq.operands.size < 2) return false
+
+    val calc = op(eq.operands[0], eq.operands[1])
+    if (eq.operands.size == 2) return eq.res == calc
+    
+    val newEq = Equation(eq.res, mutableListOf(calc).apply { addAll(eq.operands.subList(2, eq.operands.size)) })
+
+    return isSolveable1(newEq)
+}
+
+fun isSolveable1(eq : Equation) : Boolean {
+    val operators = listOf(::sum, ::prod)
+    if (eq.res < 0) return false
+
+    for (op in operators) {
+        if (solveWithOp1(eq, op))
+            return true
+    }
+
+    return false
+}
+
+fun solveWithOp2(eq : Equation, op : (Long, Long) -> Long) : Boolean {
+    if (eq.res < 0 || eq.operands.size < 2) return false
+
+    val calc = op(eq.operands[0], eq.operands[1])
+    if (eq.operands.size == 2) return eq.res == calc
+    
+    val newEq = Equation(eq.res, mutableListOf(calc).apply { addAll(eq.operands.subList(2, eq.operands.size)) })
+
+    return isSolveable2(newEq)
+}
+
+fun isSolveable2(eq : Equation) : Boolean {
+    val operators = listOf(::sum, ::prod, ::concat)
+    if (eq.res < 0) return false
+
+    for (op in operators) {
+        if (solveWithOp2(eq, op))
+            return true
+    }
+
+    return false
+}
+
 // kotlinc day07.kt -include-runtime -d day07.jar
 // java -jar day07.jar
