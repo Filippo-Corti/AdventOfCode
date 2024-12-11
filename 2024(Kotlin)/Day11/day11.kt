ce4acb19a1c9f578b60000c641964b55928b94bc
Filippo <corti.filippo03@gmail.com>
2024-12-11 12:08:13 +0100

    Day11 Part 2 (fast)

--- 2024(Kotlin)/Day11/day11.kt
+++ 2024(Kotlin)/Day11/day11.kt
@@ -3,83 +3,49 @@ import java.io.File
 fun main() {
     var stones = File("input.txt").readLines()[0].split(" ").map { it.toLong() }.toMutableList()
 
-    //println(part1(stones.toList().toMutableList()))
+    println(part1(stones))
     println(part2(stones))
 }
 
-fun part1(stones: MutableList<Long>): Int {
+val mem : MutableMap<Pair<Long, Int>, Long> = mutableMapOf()
 
-    val map : MutableMap<Long, List<Long>> = mutableMapOf()
-    
-    for (i in 0 until 5) {
-        val oldStones = stones.toList()
-        stones.clear()
-        stones.addAll(oldStones.map { blinkNTimes(map, it, 5)}.flatten())
-        println("$i/5")
-    }
-
-    return stones.size
-}
-
-fun blinkNTimes(map : MutableMap<Long, List<Long>>, stone : Long, n : Int) : List<Long> {
-
-    if (map.containsKey(stone)) {
-        //println("Skipped with map")
-        return map[stone]!!.toList()
-    }
-
-    var stones = mutableListOf(stone)
-    for (i in 0 until n) {
-        val oldStones = stones.toList()
-        var addedValues = 0
-        for (idx in oldStones.indices) {
-            val stone = oldStones[idx]
-            val stoneString = "$stone"
-            if (stone == 0L) {
-                stones[idx + addedValues] = 1
-            } else if (stoneString.length % 2 == 0) {
-                val newStone1 = stoneString.substring(0, stoneString.length / 2).toLong()
-                val newStone2 = stoneString.substring(stoneString.length / 2).toLong()
-                stones[idx + addedValues] = newStone1
-                stones.add(idx + addedValues + 1, newStone2)
-                addedValues++
-            } else {
-                stones[idx + addedValues] = stone * 2024
-            }
-        }
-    }
-
-    map[stone] = stones.toList()
-
-    return stones
-}
-
-var c = 0
-
-fun blinkNTimesList(map : MutableMap<Long, List<Long>>, stones : MutableList<Long>, count : Int, n : Int) : Long {
-    if (count == 0) 
-        return stones.size.toLong()
-
-    var sum = 0L
-    for (stone in stones) {
-        val list = blinkNTimes(map, stone, n).toMutableList()
-        sum += blinkNTimesList(map, list, count-1, n)
-        if (count == 3) {
-            println("$c/212655")
-            c++
-        }
-    }
-    return sum
+fun part1(stones: MutableList<Long>): Long {
+    return stones.map{ blinkN(it, 25) }.sum().toLong()
 }
 
 fun part2(stones: MutableList<Long>): Long {
-
-    val map : MutableMap<Long, List<Long>> = mutableMapOf()
-
-    return blinkNTimesList(map, stones, 5, 15)
+    return stones.map{ blinkN(it, 75) }.sum().toLong()
 }
 
+fun blinkN(stone : Long, n : Int) : Long {
+    if (mem.containsKey(stone to n)) {
+        return mem[stone to n]!!
+    }
 
+    val blinkedList = blink(stone)
+    var res : Long
+    if (n == 1) {
+        res = blinkedList.size.toLong()
+    } else {
+        res = blinkedList.map{ blinkN(it, n-1) }.sum().toLong()
+    }
+        
+    mem[stone to n] = res
+    return res
+}
+
+fun blink(stone : Long) : List<Long> {
+    val stoneString = "$stone"
+    if (stone == 0L) {
+        return listOf(1)
+    } else if (stoneString.length % 2 == 0) {
+        val newStone1 = stoneString.substring(0, stoneString.length / 2).toLong()
+        val newStone2 = stoneString.substring(stoneString.length / 2).toLong()
+        return listOf(newStone1, newStone2)
+    } else {
+        return listOf(stone * 2024L)
+    }
+}
 
 // kotlinc day11.kt -include-runtime -d day11.jar
 // java -jar day11.jar
