e41c758133ab188f6b6f59977590ddc6f171da7c
Filippo <corti.filippo03@gmail.com>
2024-12-22 10:31:30 +0100

    Day22 Optimized

--- 2024(Kotlin)/Day22/day22.kt
+++ 2024(Kotlin)/Day22/day22.kt
@@ -3,6 +3,8 @@ import kotlin.math.max
 
 data class ChangeSequence(val a : Int, val b : Int, val c : Int, val d : Int)
 
+var numbersSize : Int = 0
+
 fun nextNumber(n : Long) : Long {
     var curr = n
     curr = (curr * 64L).xor(curr) % 16777216
@@ -12,9 +14,9 @@ fun nextNumber(n : Long) : Long {
     return curr
 }
 
-fun getChangesSequences(n : Long) : Map<ChangeSequence, Int> {
-    var map = mutableMapOf<ChangeSequence, Int>().withDefault { 0 }
+fun updateChangesSequences(n : Long, changesMap : MutableMap<ChangeSequence, IntArray>, idx : Int) {
     var changes = ArrayDeque<Int>()
+    var foundSequences = hashSetOf<ChangeSequence>()
     var curr = n
     for (i in 0 until 2000) {
         val next = nextNumber(curr)
@@ -27,12 +29,14 @@ fun getChangesSequences(n : Long) : Map<ChangeSequence, Int> {
         if (changes.size == 4) {
             val l = changes.toList()
             val changeSeq = ChangeSequence(l[0], l[1], l[2], l[3])
-            if (!map.containsKey(changeSeq))
-                map.put(changeSeq, price)
+            if (!foundSequences.contains(changeSeq)) {
+                val array = changesMap.getOrPut(changeSeq) { IntArray(numbersSize) { 0 } }
+                array[idx] = price
+                foundSequences.add(changeSeq)
+            }
         }
         curr = next
     }
-    return map
 }
 
 fun get2000thNumber(n : Long) : Long {
@@ -46,6 +50,7 @@ fun get2000thNumber(n : Long) : Long {
 fun main() {
 
     val numbers = File("input.txt").readLines().map { it.toLong() }
+    numbersSize = numbers.size
 
     println(part1(numbers))
     println(part2(numbers))
@@ -58,16 +63,8 @@ fun part1(numbers : List<Long>) : Long {
 }
 
 fun part2(numbers : List<Long>) : Int {
-    val sequencesMaps = numbers.map { getChangesSequences(it) }
-    val keys = sequencesMaps.map { it.keys }.flatMap { it }.toHashSet()
+    val changesMap = mutableMapOf<ChangeSequence, IntArray>().withDefault { IntArray(numbersSize) }
+    numbers.forEachIndexed { i, v -> updateChangesSequences(v, changesMap, i) }
 
-    println("Need to check ${keys.size}")
-
-    var maxRes = 0
-    for (changeSeq in keys) {
-        val res = sequencesMaps.map { it.getValue(changeSeq) }.sum()
-        maxRes = max(maxRes, res)
-    }
-
-    return maxRes
+    return changesMap.map { (_, v) -> v.sum() }.max()
 }
