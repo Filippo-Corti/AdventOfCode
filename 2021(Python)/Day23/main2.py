fdbcc035079edd1e618d3244ecdb4ae6da3fef9d
Filippo <corti.filippo03@gmail.com>
2025-02-02 10:33:58 +0100

    Introducing a Heuristic for Day23

--- 2021(Python)/Day23/main2.py
+++ 2021(Python)/Day23/main2.py
@@ -8,6 +8,16 @@ hallway_positions = [(1, 1), (1, 2), (1, 4), (1, 6), (1, 8), (1, 10), (1, 11)]
 end_state = (((2, 3), 'A'), ((2, 5), 'B'), ((2, 7), 'C'), ((2, 9), 'D'), ((3, 3), 'A'), ((3, 5), 'B'), ((3, 7), 'C'), ((3, 9), 'D'),
               ((4, 3), 'A'), ((4, 5), 'B'), ((4, 7), 'C'), ((4, 9), 'D'), ((5, 3), 'A'), ((5, 5), 'B'), ((5, 7), 'C'), ((5, 9), 'D'))
 
+def h(state):
+    return sum([steps_to_dest(val, pos) * move_cost_by_type[val] for (pos, val) in dict(state).items()])
+
+# Assuming that the amphipod will sit at the entrance of its room
+def steps_to_dest(val, start):
+    dest_col = col_by_type[val]
+    (row, col) = start
+    if dest_col == col: return 0
+    return row + abs(dest_col - col)
+
 def is_reachable(state, start, end):
     steps = 0
     (row, col) = start
@@ -41,19 +51,18 @@ def next_states(state):
     state_dict = dict(state)
     nexts = []
     for ((row, col), val) in state:
-        
-        #Check if it can move to its room
+    
+        #Check if it can move into its room
         dest_col = col_by_type[val]
         if col != dest_col:
             for checked_row in range(5, 1, -1):
                 (ok, c) = is_reachable(state_dict, (row, col), (checked_row, dest_col))
                 if ok:
                     nexts.append((move(state_dict, (row, col), (checked_row, dest_col)), c * move_cost_by_type[val]))
-                    break
+                    return nexts # If I can move an amphipod into its room I can stop checking for other moves
                 else:
-                    if (checked_row, dest_col) not in state_dict: break
-                    elif state_dict[(checked_row, dest_col)] != val: break
-                
+                    if (checked_row, dest_col) not in state_dict or state_dict[(checked_row, dest_col)] != val: break
+           
         if row != 1: #Check if it can move in the hallway
             for hallway_pos in hallway_positions:
                 (ok, c) = is_reachable(state_dict, (row, col), hallway_pos)
@@ -77,7 +86,8 @@ distances = defaultdict(lambda: inf, {start: 0})
 visited = defaultdict(lambda: False)
 
 while queue:
-    cost, current = heappop(queue)
+    _, current = heappop(queue)
+    cost = distances[current]
     if is_goal(current):
         print(cost)
         break
@@ -87,7 +97,7 @@ while queue:
         new_cost = cost + action_cost
         if not visited[next_state] and new_cost < distances[next_state]:
             distances[next_state] = new_cost
-            heappush(queue, (new_cost, next_state))
+            heappush(queue, (new_cost + h(next_state), next_state))
     
     
 
